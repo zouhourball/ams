913ce27bbcca0034580f71581f2f0e4107d91c7e
GHAOUI HAMDI <ghaoui.hamdi@gmail.com>
2021-10-13 10:08:40 +0100

    add structure

--- src/components/module-hse/index.js
+++ src/components/module-hse/index.js
@@ -2,14 +2,17 @@ import { Router, Redirect } from '@reach/router'
 import Emissions from './emissions'
 import Flaring from './flaring'
 import HSSE from './hsse'
+import FlaringDetails from './flaring-details'
 
 const HSE = () => {
   return (
 
     <Router>
       <Redirect from="/" to="/ams/hse/flaring" noThrow />
-      <Emissions path="/emissions" />
       <Flaring path="/flaring" />
+      <FlaringDetails path="/flaring/:flaringId" />
+      <Emissions path="/emissions" />
+
       <HSSE path="/hsse" />
 
     </Router>
