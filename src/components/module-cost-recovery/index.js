6e2f2fff78e10e6bf5c23ff858f420146a16fcc4
AjroudM <majdi.ajroudi@target-energysolutions.com>
2021-10-13 14:01:40 +0100

    :art: add all root

--- src/components/module-cost-recovery/index.js
+++ src/components/module-cost-recovery/index.js
@@ -1,4 +1,93 @@
+import { useState } from 'react'
+import { Button } from 'react-md'
+import Mht from '@target-energysolutions/mht'
+
+import TopBar from 'components/top-bar'
+import NavBar from 'components/nav-bar'
 const CostRecovery = () => {
-  return <div>CostRecovery</div>
+  const [currentTab, setCurrentTab] = useState(0)
+  const actions = [
+    <Button
+      key="1"
+      id="save"
+      className="top-bar-buttons-list-item-btn"
+      flat
+      primary
+      swapTheming
+      onClick={() => {}}
+    >
+      Upload Annual Cost and Expenditure Report
+    </Button>,
+    <Button
+      key="2"
+      id="save"
+      className="top-bar-buttons-list-item-btn"
+      flat
+      primary
+      swapTheming
+      onClick={() => {}}
+    >
+      Download Template
+    </Button>,
+  ]
+
+  const tabsList = [
+    'Annual Cost and Expenditure',
+    'Contract Reports',
+    'Production Lifting',
+    'Transaction Report',
+    'Affiliate',
+    'Facilities',
+  ]
+  const renderCurrentTabData = () => {
+    switch (currentTab) {
+      case 0:
+        break
+      case 1:
+        break
+      case 2:
+        break
+      case 3:
+        break
+      case 4:
+        break
+      case 5:
+      default:
+        break
+    }
+    return []
+  }
+  const renderCurrentTabConfigs = () => {
+    switch (currentTab) {
+      case 0:
+        break
+      case 1:
+        break
+      case 2:
+        break
+      case 3:
+        break
+      case 4:
+        break
+      case 5:
+      default:
+        break
+    }
+    return []
+  }
+  return (
+    <div className="module-container">
+      <TopBar title="Cost Recovery Reporting" actions={actions} />
+      <NavBar
+        tabsList={tabsList}
+        activeTab={currentTab}
+        setActiveTab={setCurrentTab}
+      />
+      <Mht
+        configs={renderCurrentTabConfigs()}
+        tableData={renderCurrentTabData()}
+      />
+    </div>
+  )
 }
 export default CostRecovery
--- src/components/module-production/index.js
+++ src/components/module-production/index.js
@@ -1,4 +1,84 @@
+import { useState } from 'react'
+import { Button } from 'react-md'
+import Mht from '@target-energysolutions/mht'
+
+import TopBar from 'components/top-bar'
+import NavBar from 'components/nav-bar'
 const Production = () => {
-  return <div>Production</div>
+  const [currentTab, setCurrentTab] = useState(0)
+  const actions = [
+    <Button
+      key="1"
+      id="save"
+      className="top-bar-buttons-list-item-btn"
+      flat
+      primary
+      swapTheming
+      onClick={() => {}}
+    >
+      Upload Daily Production Report
+    </Button>,
+    <Button
+      key="2"
+      id="save"
+      className="top-bar-buttons-list-item-btn"
+      flat
+      primary
+      swapTheming
+      onClick={() => {}}
+    >
+      Download Template
+    </Button>,
+  ]
+
+  const tabsList = [
+    'Daily Production',
+    'Monthly Production',
+    'Monthly Tracking',
+    'Oman Hydrocarbon',
+
+  ]
+  const renderCurrentTabData = () => {
+    switch (currentTab) {
+      case 0:
+        break
+      case 1:
+        break
+      case 2:
+        break
+      case 3:
+      default:
+        break
+    }
+    return []
+  }
+  const renderCurrentTabConfigs = () => {
+    switch (currentTab) {
+      case 0:
+        break
+      case 1:
+        break
+      case 2:
+        break
+      case 3:
+      default:
+        break
+    }
+    return []
+  }
+  return (
+    <div className="module-container">
+      <TopBar title="Cost Recovery Reporting" actions={actions} />
+      <NavBar
+        tabsList={tabsList}
+        activeTab={currentTab}
+        setActiveTab={setCurrentTab}
+      />
+      <Mht
+        configs={renderCurrentTabConfigs()}
+        tableData={renderCurrentTabData()}
+      />
+    </div>
+  )
 }
 export default Production
