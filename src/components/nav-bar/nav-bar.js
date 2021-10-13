12af838b8812b3fa5ceb1066dc2023bb4c416079
AjroudM <majdi.ajroudi@target-energysolutions.com>
2021-10-13 09:22:30 +0100

    :art: add nav bar

---
+++ src/components/nav-bar/nav-bar.js
@@ -0,0 +1,26 @@
+import './style.scss'
+
+const NavBar = ({ tabsList, activeTab, setActiveTab }) => {
+  return (
+    <div className="navBar">
+      <div className="navBar-list">
+        {tabsList &&
+          tabsList.map((el, index) => {
+            const id = index
+            return (
+              <div
+                key={id}
+                className={`navBar-list-title ${
+                  activeTab === index ? 'active' : ''
+                }`}
+                onClick={() => setActiveTab && setActiveTab(index)}
+              >
+                {el}
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
+}
+export default NavBar
