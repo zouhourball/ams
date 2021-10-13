b420ea958680fc387910f33dbe7ffe012b31921f
AjroudM <majdi.ajroudi@target-energysolutions.com>
2021-10-13 10:57:35 +0100

    :art: add hsse module

--- src/components/module-hse/emissions.js
+++ src/components/module-hse/emissions.js
@@ -1,4 +1,74 @@
+import { useState } from 'react'
+import { Button } from 'react-md'
+import Mht from '@target-energysolutions/mht'
+
+import TopBar from 'components/top-bar'
+import NavBar from 'components/nav-bar'
 const Emissions = () => {
-  return <div>Emissions</div>
+  const [currentTab, setCurrentTab] = useState(0)
+  const actions = [
+    <Button
+      key="1"
+      id="save"
+      className="top-bar-buttons-list-item-btn"
+      flat
+      primary
+      swapTheming
+      onClick={() => {}}
+    >
+      Upload Monthly Emissions Report
+    </Button>,
+    <Button
+      key="2"
+      id="save"
+      className="top-bar-buttons-list-item-btn"
+      flat
+      primary
+      swapTheming
+      onClick={() => {}}
+    >
+      Download Template
+    </Button>,
+  ]
+
+  const tabsList = ['Monthly Report']
+  const renderCurrentTabData = () => {
+    switch (currentTab) {
+      case 1:
+        break
+      case 2:
+        break
+      case 0:
+      default:
+        break
+    }
+    return []
+  }
+  const renderCurrentTabConfigs = () => {
+    switch (currentTab) {
+      case 1:
+        break
+      case 2:
+        break
+      case 0:
+      default:
+        break
+    }
+    return []
+  }
+  return (
+    <div className="module-container">
+      <TopBar title="Emissions" actions={actions} />
+      <NavBar
+        tabsList={tabsList}
+        activeTab={currentTab}
+        setActiveTab={setCurrentTab}
+      />
+      <Mht
+        configs={renderCurrentTabConfigs()}
+        tableData={renderCurrentTabData()}
+      />
+    </div>
+  )
 }
 export default Emissions
--- src/components/module-hse/index.js
+++ src/components/module-hse/index.js
@@ -3,6 +3,8 @@ import Emissions from './emissions'
 import Flaring from './flaring'
 import HSSE from './hsse'
 import FlaringDetails from './flaring-details'
+import HsseDetails from './hsse-details'
+import EmissionsDetails from './emissions-details'
 
 const HSE = () => {
   return (
@@ -10,9 +12,11 @@ const HSE = () => {
       <Redirect from="/" to="/ams/hse/flaring" noThrow />
       <Flaring path="/flaring" />
       <FlaringDetails path="/flaring/:flaringId" />
-      <Emissions path="/emissions" />
-
       <HSSE path="/hsse" />
+      <HsseDetails path="/hsse/:hsseId" />
+      <Emissions path="/emissions" />
+      <EmissionsDetails path="/emissions/:emissionId" />
+
     </Router>
   )
 }
