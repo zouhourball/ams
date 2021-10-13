2d7469a06b00c75c6025b715d3a2445442b2bca8
ghofranetorii <ghofranetorii@gmail.com>
2021-10-13 14:00:56 +0100

    UI/UX , features fixed

--- src/components/module-production/index.js
+++ src/components/module-production/index.js
@@ -1,9 +1,7 @@
-import SupportedDocument from 'components/supported-document'
-
 const Production = () => {
   return (
     <div>
-      <SupportedDocument onSaveUpload={(files, filesToDelete) => { console.log('files:', files, 'files to delete:', filesToDelete, 'here') }}/>
+      Production
     </div>
   )
 }
--- src/components/supported-document/supported-document.js
+++ src/components/supported-document/supported-document.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { FontIcon, Button } from 'react-md'
 
 import Dropzone from 'react-dropzone'
@@ -8,43 +8,19 @@ import { fileManagerUpload } from 'libs/api'
 import './style.scss'
 
 const SupportedDocument = ({ oldFiles, onDiscard, onSaveUpload, accept, isLoading }) => {
-  const [files, setFiles] = useState([])
+  const [files, setFiles] = useState([...oldFiles])
   const [filesToDelete, setFilesToDelete] = useState([])
 
-  useEffect(() => {
-    if (oldFiles && oldFiles.length > 0) { setFiles(...files, oldFiles) }
-  }, [oldFiles])
+  // useEffect(() => {
+  //   if (oldFiles && oldFiles.length > 0) { setFiles([...files, ...oldFiles]) }
+  // }, [oldFiles])
 
   const onUpload = documents => {
     fileManagerUpload(documents).then(res => {
       if (res?.files) {
-        setFiles([...res?.files])
+        setFiles([...files, ...res?.files])
       }
     })
-  }
-
-  const renderFiles = () => {
-    return (
-      files?.map((file) => {
-        return (
-          <div key={file.id}>
-            {
-              !filesToDelete.includes(file.id) &&
-              <div>
-                <FontIcon
-                  icon
-                  iconClassName={customIncludes(file.filename)}
-                />
-                <span>{file.filename}</span>
-                <span>{file.size}</span>
-                <FontIcon onClick={() => {
-                  setFilesToDelete([...filesToDelete, file.id])
-                }}>delete</FontIcon>
-              </div>
-            }
-          </div>
-        )
-      }))
   }
 
   const customIncludes = (fileName) => {
@@ -56,57 +32,101 @@ const SupportedDocument = ({ oldFiles, onDiscard, onSaveUpload, accept, isLoadin
       case 'svg':
       case 'jpeg':
       case 'gif':
-        return `mdi mdi-file-image mdi-48px`
+        return `mdi mdi-file-image mdi-36px`
       case 'xlsx':
       case 'xlsm':
       case 'xml':
-        return `mdi mdi-file-excel mdi-48px`
+        return `mdi mdi-file-excel mdi-36px`
       case 'doc':
       case 'docx':
-        return `mdi mdi-file-word mdi-48px`
+        return `mdi mdi-file-word mdi-36px`
       case 'pdf':
-        return `mdi  mdi-file-pdf mdi-48px`
+        return `mdi  mdi-file-pdf mdi-36px`
       case 'zip':
-        return `mdi mdi-zip-box mdi-48px`
+        return `mdi mdi-zip-box mdi-36px`
       case 'html':
-        return `mdi mdi-language-html5 mdi-48px`
+        return `mdi mdi-language-html5 mdi-36px`
       case 'js':
-        return `mdi mdi-language-javascript mdi-48px`
+        return `mdi mdi-language-javascript mdi-36px`
       default:
-        return `mdi mdi-file mdi-48px`
+        return `mdi mdi-file mdi-36px`
     }
   }
-  console.log(files, 'files')
+
+  const renderFiles = () => {
+    return (
+      files?.map((file) => {
+        return (
+          <div className="file" key={file.id}>
+            {
+              !filesToDelete.includes(file.id) &&
+              <><div className="file-data">
+                <FontIcon
+                  icon
+                  iconClassName={customIncludes(file.filename)}
+                />
+                <div className="file-details">
+                  <span>{file.filename}</span>
+                  <span>{(file.size / 1024).toFixed(2)} MB</span>
+                </div>
+
+              </div>
+
+                <FontIcon onClick={() => {
+                  setFilesToDelete([...filesToDelete, file.id])
+                }}>delete</FontIcon>
+              </>
+            }
+          </div>
+        )
+      }))
+  }
+
   return (
     <div className="supported-document">
-      <Dropzone
-        onDrop ={onUpload}
-        accept = {accept}
-        multiple = {true}
-        className = "dropzone-logo"
-      >
+      <div className="supported-document-header">
+        <h2>Upload Supporting Documents</h2>
+      </div>
+      <div className="supported-document-body">
+        <Dropzone
+          onDrop={onUpload}
+          accept={accept}
+          multiple={true}
+          className="dropzone-logo"
+        >
 
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <FontIcon>file_upload</FontIcon>
-              <p>Drag and drop some files here, or click to select files</p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
-      {renderFiles()}
-      <div className="asset-details-form-footer">
-        <Button onClick={onDiscard} flat>
+          {({ getRootProps, getInputProps }) => (
+            <section className="supported-document-body-dropzone">
+              <div className="input-zone" {...getRootProps()}>
+                <input {...getInputProps()} />
+                <FontIcon className="delete-icon">file_upload</FontIcon>
+                <p>Drag & Drop file here or <b>Select File</b></p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        {renderFiles()}
+      </div>
+      <div className="supported-document-footer">
+        <Button
+          flat
+          className="discard-btn"
+          onClick={onDiscard}
+        >
           Discard
         </Button>
-        <Button onClick={onSaveUpload(
+        <Button
+          flat
+          swapTheming
+          primary
+          className="save-btn"
+          onClick={onSaveUpload(
           files?.filter(file =>
             !filesToDelete.map(fileToDelete => fileToDelete)?.includes(file?.id),
           ),
           filesToDelete,
-        )} flat swapTheming primary>
+          )}
+        >
           {isLoading ? <FontIcon primary iconClassName="mdi mdi-spin mdi-loading" /> : 'Upload'}
         </Button>
       </div>
