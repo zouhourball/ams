2f618059eb11ba189d800764f1b9adcd053022ad
khalil <khadhraouikhalil52@gmail.com>
2021-10-13 17:46:05 +0100

    top bar btns by current tab

--- src/components/module-hse/flaring.js
+++ src/components/module-hse/flaring.js
@@ -4,6 +4,7 @@ import Mht from '@target-energysolutions/mht'
 
 import TopBar from 'components/top-bar'
 import NavBar from 'components/nav-bar'
+import UploadReportDialog from 'components/upload-report-dialog'
 
 const Flaring = () => {
   const [currentTab, setCurrentTab] = useState(0)
@@ -42,6 +43,34 @@ const Flaring = () => {
     >Upload Annual Flaring Report
     </Button>,
   ]
+  const createActionsByCurrentTab = (actionsList = []) => {
+    return actionsList.map(btn =>
+
+      <Button
+        key="3"
+        id="save"
+        className="top-bar-buttons-list-item-btn"
+        flat
+        primary
+        swapTheming
+        onClick={() => {
+
+        }}
+      >{btn}
+      </Button>
+    )
+  }
+  const renderActionsByCurrentTab = () => {
+    switch (currentTab) {
+      case 1:
+        return createActionsByCurrentTab(['1', '1', '1'])
+      case 2:
+        return createActionsByCurrentTab(['2', '2', '2'])
+      case 0:
+      default:
+        return createActionsByCurrentTab(['0', '0', '0'])
+    }
+  }
 
   const tabsList = ['Annual Report', 'Monthly Report', 'Daily Report']
   const renderCurrentTabData = () => {
@@ -70,8 +99,10 @@ const Flaring = () => {
   }
   return (
     <div className='module-container'>
-      <TopBar title='Flaring' actions={actions} />
+      <TopBar title='Flaring' actions={renderActionsByCurrentTab()} />
       <NavBar tabsList={tabsList} activeTab={currentTab} setActiveTab={setCurrentTab}/>
+
+      <UploadReportDialog />
       <Mht configs={renderCurrentTabConfigs()} tableData={renderCurrentTabData()}/>
     </div>
   )
