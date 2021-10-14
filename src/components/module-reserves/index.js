82c585693a20d21c6cdd204cb8876b1a4321e6b4
AjroudM <majdi.ajroudi@target-energysolutions.com>
2021-10-14 17:53:17 +0100

    :art: add last details

--- src/components/module-reserves/index.js
+++ src/components/module-reserves/index.js
@@ -4,26 +4,38 @@ import Mht from '@target-energysolutions/mht'
 
 import TopBar from 'components/top-bar'
 import NavBar from 'components/nav-bar'
+import UploadReportDialog from 'components/upload-report-dialog'
+
 const Reserves = () => {
   const [currentTab, setCurrentTab] = useState(0)
+  const [showUploadRapportDialog, setShowUploadRapportDialog] = useState(false)
 
   const annualReservesReportingActionsHelper = [
-    { title: 'Upload Annual Reserves Report', onClick: () => { } },
-    { title: 'Download Template', onClick: () => { } },
+    {
+      title: 'Upload Annual Reserves Report',
+      onClick: () => setShowUploadRapportDialog(true),
+    },
+    { title: 'Download Template', onClick: () => {} },
   ]
 
   const HistoryAndForecastActionsHelper = [
-    { title: 'Upload History and Forecast Report', onClick: () => { } },
-    { title: 'Download Template', onClick: () => { } },
+    {
+      title: 'Upload History and Forecast Report',
+      onClick: () => setShowUploadRapportDialog(true),
+    },
+    { title: 'Download Template', onClick: () => {} },
   ]
 
   const AnnualResourceDetailActionsHelper = [
-    { title: 'Upload Annual Resource Detail Report', onClick: () => { } },
-    { title: 'Download Template', onClick: () => { } },
+    {
+      title: 'Upload Annual Resource Detail Report',
+      onClick: () => setShowUploadRapportDialog(true),
+    },
+    { title: 'Download Template', onClick: () => {} },
   ]
 
   const createActionsByCurrentTab = (actionsList = []) => {
-    return actionsList.map((btn, index) =>
+    return actionsList.map((btn, index) => (
       <Button
         key={'top-bar-btn-' + index}
         className="top-bar-buttons-list-item-btn"
@@ -33,9 +45,10 @@ const Reserves = () => {
         onClick={() => {
           btn.onClick()
         }}
-      >{btn?.title}
+      >
+        {btn?.title}
       </Button>
-    )
+    ))
   }
 
   const renderActionsByCurrentTab = () => {
@@ -82,7 +95,36 @@ const Reserves = () => {
     }
     return []
   }
-
+  const renderDialogData = () => {
+    switch (currentTab) {
+      case 0:
+        return {
+          title: 'Upload Annual Reserves Report',
+          optional: 'Attach Supporting Document (Optional)',
+          onClick: () => {},
+        }
+      case 1:
+        return {
+          title: 'Upload History and Forecast Report',
+          optional: 'Attach Supporting Document (Optional)',
+          onClick: () => {},
+        }
+      case 2:
+        return {
+          title: 'Upload Monthly Tracking Report',
+          optional: 'Attach Supporting Document (Optional)',
+          onClick: () => {},
+        }
+      case 3:
+        return {
+          title: 'Upload Oman Hydrocarbon Report',
+          optional: 'Attach Supporting Document (Optional)',
+          onClick: () => {},
+        }
+      default:
+        break
+    }
+  }
   return (
     <>
       <TopBar title="Reserve Reporting" actions={renderActionsByCurrentTab()} />
@@ -95,6 +137,16 @@ const Reserves = () => {
         configs={renderCurrentTabConfigs()}
         tableData={renderCurrentTabData()}
       />
+      {showUploadRapportDialog && (
+        <UploadReportDialog
+          hideDate
+          title={renderDialogData().title}
+          optional={renderDialogData().optional}
+          visible={showUploadRapportDialog}
+          onHide={() => setShowUploadRapportDialog(false)}
+          onSave={() => renderDialogData().onClick()}
+        />
+      )}
     </>
   )
 }
