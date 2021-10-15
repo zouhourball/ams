11415f7c4338b9aa2ced92eab3a6f6b948389a62
othmanK <othman.kahlani@target-energysolutions.com>
2021-10-15 11:54:41 +0100

    mht production cost recovry

---
+++ src/components/module-cost-recovery/helpers.js
@@ -0,0 +1,672 @@
+import { FileInput } from 'react-md'
+import { navigate } from '@reach/router'
+
+export const annualCostConfigs = () => [
+  {
+    label: 'Company',
+    key: 'company',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'text',
+  },
+
+  {
+    label: 'Block',
+    key: 'block',
+    width: '200',
+    type: 'text',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted Date',
+    key: 'submittedDate',
+    width: '200',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted By',
+    key: 'submittedBy',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Reference Date',
+    key: 'referenceDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Status Date',
+    key: 'statusDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Supporting Documents',
+    key: 'supportingDocuments',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => (
+      <FileInput
+        className="upload-docs-button"
+        id={row.id}
+        flat
+        primary
+        iconBefore
+        iconChildren="mdi mdi-upload"
+        label={'Upload Documents'}
+      />
+    ),
+  },
+  {
+    label: 'Status',
+    key: 'status',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => <div className={row?.status}>{row?.status}</div>,
+  },
+]
+
+export const contractReportConfigs = () => [
+  {
+    label: 'Company',
+    key: 'company',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'text',
+  },
+
+  {
+    label: 'Block',
+    key: 'block',
+    width: '200',
+    type: 'text',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted Date',
+    key: 'submittedDate',
+    width: '200',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted By',
+    key: 'submittedBy',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Reference Date',
+    key: 'referenceDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Status Date',
+    key: 'statusDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Supporting Documents',
+    key: 'supportingDocuments',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => (
+      <FileInput
+        className="upload-docs-button"
+        id={row.id}
+        flat
+        primary
+        iconBefore
+        iconChildren="mdi mdi-upload"
+        label={'Upload Documents'}
+      />
+    ),
+  },
+  {
+    label: 'Status',
+    key: 'status',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => <div className={row?.status}>{row?.status}</div>,
+  },
+]
+export const productionLiftingConfigs = () => [
+  {
+    label: 'Company',
+    key: 'company',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'text',
+  },
+
+  {
+    label: 'Block',
+    key: 'block',
+    width: '200',
+    type: 'text',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted Date',
+    key: 'submittedDate',
+    width: '200',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted By',
+    key: 'submittedBy',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Reference Date',
+    key: 'referenceDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Status Date',
+    key: 'statusDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Supporting Documents',
+    key: 'supportingDocuments',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => (
+      <FileInput
+        className="upload-docs-button"
+        id={row.id}
+        flat
+        primary
+        iconBefore
+        iconChildren="mdi mdi-upload"
+        label={'Upload Documents'}
+      />
+    ),
+  },
+  {
+    label: 'Status',
+    key: 'status',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => <div className={row?.status}>{row?.status}</div>,
+  },
+]
+export const transactionConfigs = () => [
+  {
+    label: 'Company',
+    key: 'company',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'text',
+  },
+
+  {
+    label: 'Block',
+    key: 'block',
+    width: '200',
+    type: 'text',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted Date',
+    key: 'submittedDate',
+    width: '200',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted By',
+    key: 'submittedBy',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Reference Date',
+    key: 'referenceDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Status Date',
+    key: 'statusDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Supporting Documents',
+    key: 'supportingDocuments',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => (
+      <FileInput
+        className="upload-docs-button"
+        id={row.id}
+        flat
+        primary
+        iconBefore
+        iconChildren="mdi mdi-upload"
+        label={'Upload Documents'}
+      />
+    ),
+  },
+  {
+    label: 'Status',
+    key: 'status',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => <div className={row?.status}>{row?.status}</div>,
+  },
+]
+export const affiliateConfigs = () => [
+  {
+    label: 'Company',
+    key: 'company',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'text',
+  },
+
+  {
+    label: 'Block',
+    key: 'block',
+    width: '200',
+    type: 'text',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted Date',
+    key: 'submittedDate',
+    width: '200',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted By',
+    key: 'submittedBy',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Reference Date',
+    key: 'referenceDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Status Date',
+    key: 'statusDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Supporting Documents',
+    key: 'supportingDocuments',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => (
+      <FileInput
+        className="upload-docs-button"
+        id={row.id}
+        flat
+        primary
+        iconBefore
+        iconChildren="mdi mdi-upload"
+        label={'Upload Documents'}
+      />
+    ),
+  },
+  {
+    label: 'Status',
+    key: 'status',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => <div className={row?.status}>{row?.status}</div>,
+  },
+]
+export const facilitiesConfigs = () => [
+  {
+    label: 'Company',
+    key: 'company',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'text',
+  },
+
+  {
+    label: 'Block',
+    key: 'block',
+    width: '200',
+    type: 'text',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted Date',
+    key: 'submittedDate',
+    width: '200',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Submitted By',
+    key: 'submittedBy',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+  },
+  {
+    label: 'Reference Date',
+    key: 'referenceDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Status Date',
+    key: 'statusDate',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    type: 'date',
+    dateFormat: 'DD MMM, YYYY',
+  },
+  {
+    label: 'Supporting Documents',
+    key: 'supportingDocuments',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => (
+      <FileInput
+        className="upload-docs-button"
+        id={row.id}
+        flat
+        primary
+        iconBefore
+        iconChildren="mdi mdi-upload"
+        label={'Upload Documents'}
+      />
+    ),
+  },
+  {
+    label: 'Status',
+    key: 'status',
+    width: '200',
+    icon: 'mdi mdi-spellcheck',
+    render: (row) => <div className={row?.status}>{row?.status}</div>,
+  },
+]
+export const annualCostData = [
+  {
+    id: '2656552',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '265653452',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '26565342352',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+]
+export const contractReportData = [
+  {
+    id: '2656552',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '265653452',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '26565342352',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+]
+
+export const productionLiftingData = [
+  {
+    id: '2656552',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '265653452',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '26565342352',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+]
+
+export const transactionData = [
+  {
+    id: '2656552',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '265653452',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '26565342352',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+]
+
+export const affiliateData = [
+  {
+    id: '2656552',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '265653452',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '26565342352',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+]
+export const facilitiesData = [
+  {
+    id: '2656552',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '265653452',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+  {
+    id: '26565342352',
+    company: 'PDO',
+    block: 31,
+    submittedDate: 233333098,
+    submittedBy: 'Mohamed Ahmed',
+    referenceDate: 2021,
+    statusDate: 23098873,
+    supportingDocuments: '',
+    status: 'Submitted',
+  },
+]
+export const actionsHeader = (key, id) => [
+  {
+    id: 1,
+    label: 'Delete',
+    onClick: () => {},
+  },
+  {
+    id: 2,
+    label: 'Download Annual Plan',
+    onClick: () => {},
+  },
+  {
+    id: 3,
+    label: 'Download Original File',
+    onClick: () => {},
+  },
+  {
+    id: 4,
+    label: 'View Details',
+    onClick: () => {
+      key && id && navigate(`/ams/hse/${key}/${id}`)
+    },
+  },
+  {
+    id: 5,
+    label: 'Upload Documents',
+    onClick: () => {},
+  },
+]
