19d3924c75d5665ce3c080cd38fa1f38d2dceb25
hassendabb <hassen.dabbabi@target-energysolutions.com>
2022-05-20 17:34:12 +0100

    add treadhole classes

---
+++ src/components/module-tendering/components/create-proposal/helper.js
@@ -0,0 +1,59 @@
+const meCompanies = [
+  {
+    ID: 843,
+    Name: 'OQ',
+  },
+  {
+    ID: 837,
+    Name: 'Daleel',
+  },
+  {
+    ID: 705,
+    Name: 'CCED',
+  },
+  {
+    ID: 838,
+    Name: 'ARA',
+  },
+  {
+    ID: 839,
+    Name: 'HCF',
+  },
+  {
+    ID: 840,
+    Name: 'MOGC',
+  },
+  {
+    ID: 841,
+    Name: 'APEX',
+  },
+  {
+    ID: 844,
+    Name: 'MOL',
+  },
+  {
+    ID: 849,
+    Name: 'PTO',
+  },
+  {
+    ID: 845,
+    Name: 'MEDCO',
+  },
+  {
+    ID: 846,
+    Name: 'MASIRAH OIL',
+  },
+  {
+    ID: 847,
+    Name: 'OMAN LASSO',
+  },
+  {
+    ID: 848,
+    Name: 'TYTHES TOM',
+  },
+  {
+    ID: 850,
+    Name: 'PETROLEB',
+  },
+]
+export default meCompanies
